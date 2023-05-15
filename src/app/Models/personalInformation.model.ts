bae085dec159a004539dd07d109d97ba9ac01997
hoeshi <patrick.hoescheler@hotmail.com>
2023-05-15 16:12:24 +0200

    v1.6 Formgrouperstellung und strukturierung Designanpassung Felderanpassung strukturierung

--- src/app/Models/personalInformation.model.ts
+++ src/app/Models/personalInformation.model.ts
@@ -1,5 +1,21 @@
-export class personalInformation {
-    constructor() {
-    
-    }
+export class PersonalInformation {
+    gender: string = "";
+    firstName: string = "";
+    lastName: string = "";
+    birthDate: Date = new Date();
+    address: string = "";
+    postalCode: string = "";
+    location: string = "";
+    civilStatus: string = "";
+    nationality: string = "";
+    religion: string = "";
+    homeTown: string = "";
+    phone: string = "";
+    email: string = "";
+    finmaNumber: string = "";
+    ciceroNumber: string = "";
+    permit: string = "";
+    movedLastYear: boolean = false;
+    previousAddress: string = "";
+    moveDate: Date = new Date();
 }
