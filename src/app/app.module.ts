804279d90d5f70870edd89ccaff39064faa1f512
hoeshi <patrick.hoescheler@hotmail.com>
2023-05-22 17:29:23 +0200

    v2.3 Feinschliff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
 import { BenefitsFormComponent } from './benefits-form/benefits-form.component';
@@ -17,9 +16,8 @@ import { ChildFormComponent } from './child-form/child-form.component';
 import { SideJobFormComponent } from './side-job-form/side-job-form.component';
 import { BankAndInsuranceFormComponent } from './bank-and-insurance-form/bank-and-insurance-form.component';
 import { AgencyFormComponent } from './agency-form/agency-form.component';
+import { AbilityToWorkComponent } from './ability-to-work/ability-to-work.component';
 import { AbilityToWorkFormComponent } from './ability-to-work-form/ability-to-work-form.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 
 @NgModule({
   declarations: [
@@ -32,21 +30,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SideJobFormComponent,
     BankAndInsuranceFormComponent,
     AgencyFormComponent,
+    AbilityToWorkComponent,
     AbilityToWorkFormComponent,
     
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatCardModule,
-    MatSelectModule,
-    BrowserAnimationsModule,
-    MatRadioModule,
-    ReactiveFormsModule
+    MatCardModule
     
   ],
   providers: [],
